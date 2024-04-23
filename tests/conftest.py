eebdcf75a9d3d7f14ff69a002f5729a8c5bf1175
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2024-04-23 12:52:24 +0100

    using relative paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,17 @@
-import os
+import requests
 import numpy as np
 import pytest
 
 
 @pytest.fixture
 def read_demo_data():
-    masks_in = np.load('/home/dimitris/dev/python/plane_stitcher/tests/data/masks_in.npy')
-    masks_out = np.load('/home/dimitris/dev/python/plane_stitcher/tests/data/masks_out.npy')
+    # masks_in_url = 'https://github.com/acycliq/plane_stitcher/raw/master/tests/data/masks_in.npy'
+    # r_in = requests.get(masks_in_url)
+    # f = open('masks_in.npy', 'wb')
+    # f.write(r_in.content)
+    # f.close()
+
+    masks_in = np.load('./data/masks_in.npy')
+    masks_out = np.load('./data/masks_out.npy')
+
     return masks_in, masks_out
